c25bc0b7c0a251a8976ebd143aae8b6ae45f1f5c
serhii.lomov <serhii.lomov@itomy.ch>
2025-03-07 04:36:16 +0200

    Init commit

---
+++ SLLabyrinthIOS/SLLabyrinthIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SLLabyrinthIOSApp.swift
+//  SLLabyrinthIOS
+//
+//  Created by serhii.lomov on 07.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SLLabyrinthIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SLLabyrinthIOSTests/SLLabyrinthIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SLLabyrinthIOSTests.swift
+//  SLLabyrinthIOSTests
+//
+//  Created by serhii.lomov on 07.03.2025.
+//
+
+import Testing
+@testable import SLLabyrinthIOS
+
+struct SLLabyrinthIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
